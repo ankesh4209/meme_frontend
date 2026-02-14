97d07b6e24ea1225400883d80872a0f56b15f367
Rahul <rjshah1902@gmail.com>
2026-02-15 02:08:50 +0530

    jnjn

--- src/App.jsx
+++ src/App.jsx
@@ -104,7 +104,7 @@ const App = () => {
 
           <Routes>
             {/* Auth Routes */}
-            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Login />} />
             <Route path="/register" element={<Register />} />
 
             {/* Dashboard Route */}
